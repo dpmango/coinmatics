c533c74d858dc43cce17d67383057a09f17899c8
gadmeer <ll@finistra.tech>
2020-05-04 13:38:07 +0300

    update favicons

--- gulp/tasks/images.js
+++ gulp/tasks/images.js
@@ -6,13 +6,13 @@ import config from '../config.js';
 
 const task = () =>
   gulp
-    .src([config.src.img + '/**/*.{jpg,png,jpeg,svg,gif}'])
+    .src([config.src.img + '/**/*.{jpg,png,jpeg,svg,gif,ico}'])
     .pipe(config.production ? cache(imagemin({ interlaced: true })) : util.noop())
     .pipe(gulp.dest(config.dest.img));
 
 const buildImages = () => task();
 const watch = () => () => {
-  gulp.watch([config.src.img + '/**/*.{jpg,png,jpeg,svg,gif}'], buildImages);
+  gulp.watch([config.src.img + '/**/*.{jpg,png,jpeg,svg,gif,ico}'], buildImages);
 };
 
 module.exports.build = buildImages;
