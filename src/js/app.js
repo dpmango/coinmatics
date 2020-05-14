e6293e69822fc7c3383ecd24a3deff7968a202ce
Sergey Khmelevskoy <xs290@me.com>
2020-05-14 16:05:53 +0300

    roadmap

--- src/js/app.js
+++ src/js/app.js
@@ -78,6 +78,7 @@ var easingSwing = [0.02, 0.01, 0.47, 1]; // default jQuery easing
       APP.Plugins.Scaler.init(fromPjax);
       APP.Plugins.Chart.init(fromPjax);
       APP.Plugins.Sticky.init(fromPjax);
+      APP.Plugins.LAYOUT.init(fromPjax);
 
       // APP.Plugins.Ymaps.init();
 
